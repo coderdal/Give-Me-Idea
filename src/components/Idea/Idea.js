e455c45e7c0a90aa68561e06de3cf1ed72cac165
coderdal <agircoder@gmail.com>
2022-05-25 19:13:46 +0300

    Idea page design

--- src/components/Idea/Idea.js
+++ src/components/Idea/Idea.js
@@ -5,6 +5,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import styles from "./Idea.module.css";
 
 import axios from "axios";
+import { ArrowBackIcon } from "../assets/icons";
 
 const Idea = () => {
   const navigate = useNavigate();
@@ -18,7 +19,6 @@ const Idea = () => {
       try {
         const response = await axios.get(`http://localhost:3001/idea/${id}`);
         await setIdea(response.data[0]);
-        console.log(response);
         await setIsLoading(false);
       } catch (error) {
         console.error(error);
@@ -28,14 +28,38 @@ const Idea = () => {
     getData();
   }, [id]);
 
+  window.scrollTo(0, 0);
+
   return isLoading ? (
     <h1>Loading</h1>
   ) : (
-    <section className={styles.idea}>
-      <button type="submit" onClick={() => navigate(-1)}>
-        go back
-      </button>
-      <h2>{idea.title}</h2>
+    <section className={styles.ideaSection}>
+      <div className={styles.previousPage} onClick={() => navigate(-1)}>
+        <ArrowBackIcon />
+        Go Back
+      </div>
+      <div className={styles.idea}>
+        <div className={styles.image}>
+          <img src={idea.image} alt={idea.title} />
+        </div>
+        <div className={styles.details}>
+          <h1>{idea.title}</h1>
+          <p>{idea.description}</p>
+          <p>{idea.note}</p>
+          <h3>Links may be useful:</h3>
+          <ul>
+            {idea.related.map((link, index) => {
+              return (
+                <li key={index}>
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    {link}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/assets/icons/index.js
+++ src/components/assets/icons/index.js
@@ -251,6 +251,20 @@ const ClickIcon = () => {
   );
 };
 
+const ArrowBackIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      viewBox="0 0 24 24"
+      fill="#fff"
+    >
+      <path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm7.58 0l5.988-5.995 1.414 1.416-4.574 4.579 4.574 4.59-1.414 1.416-5.988-6.006z" />
+    </svg>
+  );
+};
+
 export {
   CodeIcon,
   MenuIcon,
@@ -265,4 +279,5 @@ export {
   LinkedinIcon,
   GithubIcon,
   ClickIcon,
+  ArrowBackIcon,
 };
